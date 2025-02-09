98db8d5dc31ce4ba13d29591c5fbb052a3d0762d
federicodececco <federicodececco1@gmail.com>
2025-02-09 11:10:26 +0100

    add:partial patch

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useTransition } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import axios from 'axios'
 import UserRow from './components/UserRow'
 
 function App() {
-  const [user, setUser] = useState({
+  const startState = {
     name: '',
     id: '',
     author: '',
     content: '',
     role: 'FrontEnd',
     visible: true,
-  })
-  const [usersList, setUserList] = useState([''])
+  }
+
+  const [user, setUser] = useState(startState)
+  const [usersList, setUserList] = useState([])
+
+  //devo cambaire il fetch, dato che non carico davvero i dati sul server
   function fetchUsers() {
     axios.get('http://localhost:3000/users/').then(res => setUserList(res.data))
-    console.log(user)
+    console.log('fetch')
   }
 
   const handleFormField = (fieldName, value) => {
@@ -26,25 +31,51 @@ function App() {
   const handleSubmit = e => {
     e.preventDefault()
 
-    console.log(user)
-
-    axios.post('http://localhost:3000/users/', user).then(fetchUsers)
-    console.log(usersList)
-    setUser(currentUser => {
-      return {
-        name: '',
-        id: '',
-        author: '',
-        content: '',
-        role: 'FrontEnd',
-        visible: 'true',
-      }
-    })
+    axios
+      .post('http://localhost:3000/users/', user)
+      .then(() => fetchUsers())
+      .then(() => setUser(startState))
   }
+
   const removeUser = id => {
     console.log(id)
-    axios.delete(`http://localhost:3000/users/${id}`).then(fetchUsers)
+    axios.delete(`http://localhost:3000/users/${id}`).then(() => fetchUsers)
   }
+
+  const modifyUser = id => {
+    let patchUser = {}
+    axios
+      .get(`http://localhost:3000/users/${id}`)
+      .then(response => {
+        const currentUser = response.data
+        patchUser = {
+          name: user.name !== startState.name ? user.name : currentUser.name,
+          author:
+            user.author !== startState.author
+              ? user.author
+              : currentUser.author,
+          content:
+            user.content !== startState.content
+              ? user.content
+              : currentUser.content,
+          role: user.role !== startState.role ? user.role : currentUser.role,
+          visible:
+            user.visible !== startState.visible
+              ? user.visible
+              : currentUser.visible,
+        }
+
+        return axios.patch(`http://localhost:3000/users/${id}`, patchUser)
+      })
+      .then(() => fetchUsers())
+      .then(() => setUser(startState))
+
+    axios
+      .patch('http://localhost:3000/users/' + id, { patchUser })
+      .then(() => fetchUsers())
+      .then(() => setUser(startState))
+  }
+
   useEffect(fetchUsers, [])
 
   return (
@@ -57,6 +88,7 @@ function App() {
                 key={elem.id}
                 user={elem}
                 remove={() => removeUser(elem.id)}
+                modify={() => modifyUser(elem.id)}
               ></UserRow>
             )
           })}
@@ -93,7 +125,7 @@ function App() {
           <input
             name='visible'
             type='checkbox'
-            checked={user.visible}
+            checked={true}
             onChange={event => {
               handleFormField('visible', event.target.checked)
             }}
